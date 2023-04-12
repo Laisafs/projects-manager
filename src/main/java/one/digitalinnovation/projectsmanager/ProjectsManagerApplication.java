c9fb2147e72fc5105c0106808e3136846e4cba59
Laisa <laisafs10@gmail.com>
2023-04-12 15:42:07 -0300

    commit inicial

---
+++ src/main/java/one/digitalinnovation/projectsmanager/ProjectsManagerApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.projectsmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectsManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectsManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/one/digitalinnovation/projectsmanager/ProjectsManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.projectsmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectsManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
